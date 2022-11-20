2b335b3ff1f6e47aa228976e1b6209d1ef5711fb
marcobuontempo <marco.buon36@gmail.com>
2022-11-20 21:21:32 +1100

    Create /songs/find endpoint

--- backend/controllers/songs-controller.js
+++ backend/controllers/songs-controller.js
@@ -1,4 +1,5 @@
 const SpotifyModel = require('../models/spotify-model')
+const ObjectId = require('mongodb').ObjectId;
 
 // Sends all songs data stored in DB
 module.exports.getAllSongs = async (req, res, next) => {
@@ -6,6 +7,9 @@ module.exports.getAllSongs = async (req, res, next) => {
   res.status(200).send(docs)
 }
 
-
-
-
+// Sends the doc in DB matching all the input _ids
+module.exports.getSongByIds = async (req, res, next) => {
+  const objIds = req.body.gif_ids.map(id => ObjectId(id))
+  const doc = await SpotifyModel.findOne({ gif_ids: { "$all": objIds } })
+  res.status(200).send(doc)
+}
--- backend/routes/songs.js
+++ backend/routes/songs.js
@@ -6,4 +6,9 @@ var controller = require("../controllers/songs-controller")
 /* GET all songs. */
 router.get('/', controller.getAllSongs);
 
+/* GET song by matching all _ids in req JSON body. 
+   body = { "ids" : ["idxxx", "idxxxx", "idxxxx" ] }
+*/
+router.get('/find', controller.getSongByIds);
+
 module.exports = router;
